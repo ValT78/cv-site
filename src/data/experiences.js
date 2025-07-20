d64a2ea036f04838be8a1ccec6cdb3b21239c7bb
Valt7 <v.lantigny@gmail.com>
2025-07-20 18:43:30 +0200

    un peu d'expérience

--- src/data/experiences.js
+++ src/data/experiences.js
@@ -3,9 +3,9 @@ export const experiences = [
     id: 'cns',
     title: 'CNS Communications – Alternance',
     dates: 'Octobre 2024 – Maintenant',
-    teaser: "J'ai développé une plateforme d'observabilité complète, Open source, sur Kubernetes",
+    teaser: "J'ai développé une plateforme d'observabilité Open source sur Kubernetes",
     companyLogo: '/assets/logos/cns.png',
-    tags: ['Kubernetes', 'ArgoCD', 'Grafana', 'ELK'],
+    tags: ['Kubernetes', 'ArgoCD', 'Opentelemetry', 'Grafana', 'Prometheus', 'Python'],
     githubLink: 'https://github.com/ValT78/observability-platform',
     images: [
       '/assets/exp/cns-dashboard.png',
@@ -13,16 +13,16 @@ export const experiences = [
     ],
     details: [
       {
-        title: 'Contexte',
-        text: "L'objectif était de récupérer l'ENSEMBLE des métriques, traces et logs de l'entreprise, de les uniformiser et de les visualiser sur une plateforme centralisée. <br>Cela permet par exemple de correler les données des firewalls avec leur logs sur le même Dashboard. <br>Initialement, tout était dispersé sur divers outils incompatibles.",
+        title: 'Objectif',
+        text: "Récupérer <strong>l'ENSEMBLE</strong> des <u>métriques</u>, <u>traces</u> et <u>logs</u> de l'entreprise, de les uniformiser et de les visualiser sur une plateforme <strong>centralisée</strong>. <br>Cela permet par exemple de correler les données des firewalls avec leur logs sur le même Dashboard. <br>Initialement, tout était dispersé sur divers outils incompatibles.",
       },
       {
         title: 'Une plateforme Open Source',
-        text: "J'ai utilisé OpenTelemetry pour la collecte des données, Grafana pour la visualisation, Loki et Promtail pour les logs, et Prometheus pour les métriques. J'ai également intégré ArgoCD pour le déploiement continu de l'infrastructure.",
+        text: "<u>OpenTelemetry</u> et Alloy m'ont permis de collecter et transformer les données pour les rendre compatibles entre elles.<br><u>Grafana</u> pour la visualisation.<br><u>Prometheus</u> et <u>Opensearch</u> pour le stockage (et l'analyse des données).<br>Des scripts <u>Python</u> m'ont permis d'utiliser les APIs des outils tiers pour la collecte.",
       },
       {
         title: 'Déploiement sur Kubernetes',
-        text: "J'ai créé l'ensemble des manifests Kubernetes en suivant la philosophie GitOps avec ArgoCD. J'ai également utilisé les Helms charts officiels pour les outils comme Grafana, Loki, Promtail, etc.",
+        text: "La plateforme fonctionne sur Kubernetes en respectant la philosophie <strong>GitOps</strong> avec <u>ArgoCD</u>. Je suis passé par les Helms charts officiels quand c'était possible pour déployer les différents outils.",
       }
     ]
   },
@@ -30,9 +30,9 @@ export const experiences = [
     id: 'cern',
     title: 'CERN – Stage international',
     dates: 'Juin – Septembre 2024',
-    teaser: 'Automatisation et monitoring de systèmes à très grande échelle.',
+    teaser: 'Automatisation et monitoring pour compresser le cloud du CERN',
     companyLogo: '/assets/logos/cern.png',
-    tags: ['Puppet', 'Linux', 'Prometheus', 'Python'],
+    tags: ['Linux', 'Bash', 'Grafana', 'Puppet'],
     githubLink: 'https://github.com/ValT78/cern-automation',
     images: [
       '/assets/exp/cern-infra.png',
@@ -40,8 +40,8 @@ export const experiences = [
     ],
     details: [
       {
-        title: 'Infrastructure CERN',
-        text: 'Refonte d’une partie de l’automatisation avec Puppet, scripts Bash et Python pour plus de flexibilité sur des nœuds critiques.'
+        title: 'Compresser 8Po de données',
+        text: "Une nouvelle fonctionnalité de <strong>compression</strong> a été ajoutée sur l'outil utilisé pour gérer les données de scientifiques. Mon rôle était de l'implémenter et d'automatiser la compression des donnéees via du scripting Bash.",
       },
       {
         title: 'Monitoring à échelle',
